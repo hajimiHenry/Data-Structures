2341c95af59c9a99f6b5f03865c2e9251ae5b6a1
hajimiHenry <spaner2022@outlook.com>
2026-06-27 11:29:00 +0800

    更改了gitignore 并且删除了一些垃圾

--- 2.1.c
+++ 2.1.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
 
 void swap(int *a, int *b);
-
+void double_it(int *p);
+void set_null(int **pp);
+void reverse(int *a, int n);
+void bubble(int a[], int n);
 
 int main(void)
 {
@@ -18,11 +21,67 @@ int main(void)
     // swap(&a, &b);
     // printf("%d %d", a, b);
 
+    // int a, b;
 
+    // scanf("%d", &a);
+    // int *p = &a;
 
-    
+    // double_it(&a);
+    // printf("%d\n", a);
 
+    // set_null(&p);
+    // printf("%p %d\n", p, a);
+
+    int a[] = {3, 2, 7, 6, 4};
+
+    bubble(a, 5);
     return 0;
+}
+
+void bubble(int a[], int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n-1-i; j++)
+        {
+            printf("a[%d] == %d , a[%d]== %d \n",i ,a[i],j+1,a[j+1]);
+            if (a[j] >= a[j+1])
+            {
+                int tmp = a[j];
+                a[j] = a[j+1];
+                a[j+1] = tmp;
+                
+                /* code */
+            }
+            
+            /* code */
+        }
+
+        /* code */
+    }
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d\n", a[i]);
+        /* code */
+    }
+}
+
+
+
+
+void reverse(int *a, int n)
+{
+    for (int i = 0; i < n / 2; i++)
+    {
+        int tmp = a[i];
+        a[i] = a[n - 1 - i];
+        a[n - 1 - i] = tmp;
+    }
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d\n", a[i]);
+        /* code */
+    }
 }
 
 void swap(int *a, int *b)
@@ -32,6 +91,12 @@ void swap(int *a, int *b)
     *b = t;
 }
 
-void double_it(int *p){
-    
+void double_it(int *p)
+{
+    *p *= 2;
+}
+
+void set_null(int **pp)
+{
+    *pp = NULL;
 }
